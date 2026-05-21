70a8459f7ba747e2bc8461e35b6686795ee8c0b7
MostafaSalama108 <mostafa.slamaa1998@gmail.com>
2026-05-21 19:12:06 +0300

    first-commit main

---
+++ frontend/src/pages/Dashboard/Projects.jsx
@@ -0,0 +1,9 @@
+import {Outlet} from "react-router-dom";
+function Projects() {
+    return (
+    <section className="py-4"> 
+        <Outlet />
+    </section>
+    );
+}
+export default Projects;
